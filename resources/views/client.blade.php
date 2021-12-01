aee911e646935818e1e3d4a8730d2f697e5507da
cristian1304 <codresanucristian@gmail.com>
2021-12-01 11:52:28 +0200

    I fixed all bugs for clients page

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -13,9 +13,9 @@
 
             <!-- CALENDAR HEADER -->
             <div class="d-flex justify-content-center align-items-center">
-                <h1><a href="/{{ $calendar['monthNow']-1 }}">&larr;</a></h1>
+                <h1><a href="/{{ $calendar['monthDigit']-1 }}-{{ $calendar['year'] }}">&larr;</a></h1>
                 <h2>{{ $calendar['month']}} {{ $calendar['year'] }}</h2>
-                <h1><a href="/{{ $calendar['monthNow']+1 }}">&rarr;</a></h1>
+                <h1><a href="/{{ $calendar['monthDigit']+1 }}-{{ $calendar['year'] }}">&rarr;</a></h1>
             </div>
 
 
@@ -31,19 +31,19 @@
                         <th>Friday</th>
                         <th>Saturday</th>
                     </tr>
-                    @for ($day=1-$calendar['skipDays'], $dayBox=1; $day<=$calendar['daysInMonth']; $day++, $dayBox++)
-                        @if (($dayBox % 7 == 1) && ($day >= $calendar['skipDays']))
-                            <tr><td id="{{ $day }}">{{ $day }}</td>
+                    @for ($day=1-$calendar['skipDays'], $dayTile=1; $day<=$calendar['daysInMonth']; $day++, $dayTile++)
+                        @if (($dayTile % 7 == 1) && ($day >= $calendar['skipDays']))
+                            <tr><td value="not-allowed" id="{{ $day }}">{{ $day }}</td>
                         @elseif ($day < 1)
-                            @if ($dayBox == 1)
-                                <tr><td></td>
+                            @if ($dayTile == 1)
+                                <tr><td value="not-allowed"></td>
                             @else
-                                <td></td>
+                                <td value="not-allowed"></td>
                             @endif
-                        @elseif ($dayBox % 7 == 0)
-                            <td id="{{ $day }}">{{ $day }}</td></tr>
+                        @elseif ($dayTile % 7 == 0)
+                            <td value="not-allowed" id="{{ $day }}">{{ $day }}</td></tr>
                         @else
-                            <td id="{{ $day }}">{{ $day }}</td>
+                            <td value="allowed" id="{{ $day }}">{{ $day }}</td>
                         @endif
                     @endfor
                 </table>
@@ -51,7 +51,8 @@
         </div>
 
         <!-- FORM -->
-        <div class="container mt-5">
+        <div class="container mt-5 border border-black">
+            <h1 class="text-center">Form</h1>
             <form method="POST" action="/appointment"> 
                 @csrf
                 <input type="text" name="name" placeholder="Your name">
@@ -65,15 +66,7 @@
                     <option value="3">Contractor 3</option>
                 </select>
                 @error('contractor') <div style="color:red">{{ $message }}</div> @enderror
-                <div class="container d-flex border border-black flex-wrap justify-content-around">
-                    <p><input type="checkbox" name="services1"> Services 1</p>
-                    <p><input type="checkbox" name="services2"> Services 2</p>
-                    <p><input type="checkbox" name="services3"> Services 3</p>
-                    <p><input type="checkbox" name="services4"> Services 4</p>
-                    <p><input type="checkbox" name="services5"> Services 5</p>
-                    <p><input type="checkbox" name="services6"> Services 6</p>            
-                </div>
-                <input type="text" name="date" placeholder="Choose the date">
+                <input type="text" name="date" id="date" placeholder="Choose the date" readonly>
                 @error('date') <div style="color:red">{{ $message }}</div> @enderror
                 <select name="hour">
                     <option value="0" selected disabled>Choose hour</option>
@@ -87,7 +80,7 @@
                     <option value="8">16</option>
                     <option value="9">17</option>
                 </select>
-                @error('date') <div style="color:red">{{ $message }}</div> @enderror
+                @error('hour') <div style="color:red">{{ $message }}</div> @enderror
                 <select name="minute">
                     <option value="0" selected disabled>Choose minute</option>
                     <option value="1">00</option>
@@ -104,6 +97,16 @@
                     <option value="12">55</option>
                 </select>
                 @error('minute') <div style="color:red">{{ $message }}</div> @enderror
+                
+                <div class="container d-flex flex-wrap justify-content-around">
+                    <p id="service-text-1"><input type="checkbox" id="service-1" name="services"> Service 1</p>
+                    <p id="service-text-2"><input type="checkbox" id="service-2" name="services"> Service 2</p>
+                    <p id="service-text-3"><input type="checkbox" id="service-3" name="services"> Service 3</p>
+                    <p id="service-text-4"><input type="checkbox" id="service-4" name="services"> Service 4</p>
+                    <p id="service-text-5"><input type="checkbox" id="service-5" name="services"> Service 5</p>
+                    <p id="service-text-6"><input type="checkbox" id="service-6" name="services"> Service 6</p>            
+                </div>
+                @error('services') <div style="color:red">{{ $message }}</div> @enderror
                 <button type="submit" class="btn btn-primary">Make Appointment</button>
             </form>
         </div>
@@ -113,32 +116,81 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="js/myscript.js"></script>
-        <script>
+        <script type="text/javascript">
             $(document).ready(function(){
                 var calendar = {!! json_encode($calendar) !!};
-                var date = new Date();
-                var month = date.getMonth()+1;
+                var current_month = new Date().getMonth()+1;
+                var current_year = new Date().getFullYear();
+                
 
+                // MARK TODAY'S TILE
+                for(let day=1; day<=calendar['daysInMonth']; day++)
+                    if (((day == calendar['today']) && (current_month == calendar['monthDigit'])) && (current_year == calendar['year']))
+                        $("#"+day).css({'background':'cornflowerblue', 'color':'white', 'font-weight':'bold'});
 
-                // SHOW CURRENT DAY IN CALENDAR
-                if (calendar['monthNow'] == month)
-                    $("#"+calendar['today']).css({'background':'cornflowerblue', 'color':'white', 'font-weight':'bold'});
 
 
                 // CHANGE THE CURSOR ACCORDING TO THE VALIDITY OF DAYS
                 for (let day=1; day<=calendar['daysInMonth']; day++)
-                    if (calendar['monthNow'] == month)
-                    {
-                        if (day < calendar['today']) 
-                            $("#"+day).css({'cursor':'not-allowed'});
-                        else                      
-                            $("#"+day).css({'cursor':'pointer'});
-                    }
-                    else if (calendar['monthNow'] < month)
+                {   
+                    if (calendar['year'] < current_year){
                         $("#"+day).css({'cursor':'not-allowed'});
-                    else if (calendar['monthNow'] > month)
+                        $("#"+day).attr("value","not-allowed");
+                    }
+                    else if (calendar['year'] == current_year){
+                        if (calendar['monthDigit'] < current_month){
+                            $("#"+day).css({'cursor':'not-allowed'});
+                            $("#"+day).attr("value","not-allowed");
+                        }
+                        else if ((calendar['monthDigit'] == current_month) && (day < calendar['today'])){
+                            $("#"+day).css({'cursor':'not-allowed'});
+                            $("#"+day).attr("value","not-allowed");
+                        }
+                        else{
+                            $("#"+day).css({'cursor':'pointer'});
+                            $("#"+day).attr("value","allowed");
+                        }
+                    }
+                    else{
                         $("#"+day).css({'cursor':'pointer'});
+                        $("#"+day).attr("value","allowed");
+                    }
+                }
 
+
+
+                // CLICK AND HOVER TILES
+                $("td").click(function(){
+                    if ($(this).attr("value") == "allowed")
+                        $("#date").val(($(this).attr("id")+" "+calendar['month']+" "+calendar['year']));
+                });
+
+                $("td").mouseenter(function(){
+                    if ($(this).attr("value") == "allowed")
+                        $("#"+$(this).attr("id")).css({'background':'#3366ff', 'color':'white', 'font-weight':'bold'});
+                });
+
+                $("td").mouseleave(function(){
+                    if ($(this).attr("value") == "allowed"){
+                        if ((($(this).attr("id") == calendar['today']) && (current_month == calendar['monthDigit'])) && (current_year == calendar['year']))
+                            $("#"+$(this).attr("id")).css({'background':'cornflowerblue', 'color':'white', 'font-weight':'bold'});
+                        else
+                            $("#"+$(this).attr("id")).css({'background':'whitesmoke', 'color':'black', 'font-weight':'normal'});
+                    }
+                });
+
+
+                
+                // GROUPING CHECKBOXES TO SEND A SINGLE VALUE TO THE DATABASE
+                $('input[type=checkbox]').click(function(){
+                    var service_value = '';
+                    
+                    for (let i=1; i<=$('input[type=checkbox]').length; i++)
+                        if ($("#service-"+i).is(':checked'))
+                            service_value = service_value + $("#service-text-"+i).text()+"\n";
+                    
+                    $("input[type=checkbox]").attr("value", service_value);
+                });
             });
         </script>
     </body>
